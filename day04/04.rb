a9ff68aeb8b81d2af45fecd147c014479b667760
david <davidskim4@gmail.com>
2017-02-05 16:27:29 -0600

    day 4 - part 2

--- day04/04.rb
+++ day04/04.rb
@@ -8,16 +8,36 @@ def checksum_by_occurrences(string)
     .join
 end
 
-result = 0
-File.readlines("04.input").each do |line|
+Room = Struct.new(:encrypted_name, :sector_id, :checksum)
+
+rooms = File.readlines("04.input").map { |line|
   tokens = line.split(/[-\[\]\n]/)
-  encrypted_names = tokens.slice(0..-3).join
+  encrypted_name = tokens.slice(0..-3).join
   sector_id = tokens.slice(-2).to_i
   checksum = tokens.slice(-1)
+  Room.new(encrypted_name, sector_id, checksum)
+}
 
-  if checksum_by_occurrences(encrypted_names) == checksum
-    result += sector_id
+real_rooms = rooms.select { |r| checksum_by_occurrences(r.encrypted_name) == r.checksum }
+
+# Part 1
+sum = real_rooms.map(&:sector_id).reduce(:+)
+puts "Part 1: sum of sector IDs of the real rooms: #{sum}"
+
+# Part 2
+DecryptedRoom = Struct.new(:decrypted_name, :sector_id)
+class ShiftCipher
+  def self.decrypt(letters, sector_id)
+    DecryptedRoom.new(_rotate(letters, sector_id), sector_id)
+  end
+
+  def self._rotate(letters, times)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letters.tr(alphabet, alphabet.chars.rotate(times % 26).join)
   end
 end
 
-puts "Sum of sector IDs of the real rooms: #{result}"
+decrypted_rooms = real_rooms.map { |r| ShiftCipher.decrypt(r.encrypted_name, r.sector_id.to_i) }
+north_pole_room = decrypted_rooms.select { |r| r.decrypted_name.include?("north") }.first
+puts "Part 2: decrypted room's sector_id with north pole presents #{north_pole_room.sector_id}"
+
