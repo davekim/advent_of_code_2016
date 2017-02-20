3ef5f632842d47f42538097f9e7eb26d7e98ea23
david <davidskim4@gmail.com>
2017-02-19 22:29:19 -0600

    Day 7 - part 2

--- day07/07.rb
+++ day07/07.rb
@@ -1,11 +1,36 @@
-def abba?(string_to_check)
-  string_to_check.chars.each_cons(4) do |s|
+def abba?(word)
+  word.chars.each_cons(4) do |s|
     return true if (s[0] + s[1] == s[3] + s[2]) && (s[3] != s[2])
   end
   return false
 end
 
-count = 0
+def find_abas(word)
+  abas = []
+  word.chars.each_cons(3) do |s|
+    if aba?(s)
+      abas << s.join
+    end
+  end
+  abas
+end
+
+def aba?(s)
+  (s[0] == s[2]) && (s[0] != s[1])
+end
+
+def bab?(word, abas)
+  word.chars.each_cons(3) do |s|
+    abas.each do |aba|
+      return true if (s[0] == aba[1]) && (s[1] == aba[0]) && (s[2] == aba[1])
+    end
+  end
+  return false
+end
+
+tls_count = 0
+ssl_count = 0
+
 File.readlines("07.input").each do |line|
   outside_with_i, inside_with_i = line.chomp.split(/[\[\]]/).each_with_index.partition { |c, index| index % 2 == 0 }
 
@@ -13,8 +38,14 @@ File.readlines("07.input").each do |line|
   inside = inside_with_i.map(&:first)
 
   if (outside.any? { |word| abba?(word) }) && (inside.none? { |word| abba?(word) })
-    count += 1
+    tls_count += 1
+  end
+
+  abas = outside.map { |word| find_abas(word) }.flatten
+  if inside.any? { |word| bab?(word, abas) }
+    ssl_count += 1
   end
 end
 
-puts "Count #{count}"
+puts "Part 1: TLS count #{tls_count}"
+puts "Part 2: SSL count #{ssl_count}"
