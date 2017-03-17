3c18b2888618a190eca10b8bc1b53c1367d5e52d
david <davidskim4@gmail.com>
2017-03-17 00:12:48 -0500

    Day 10 part 2

--- day10/10.rb
+++ day10/10.rb
@@ -20,18 +20,12 @@ class Bot
   end
 end
 
-class Transaction
-  attr_reader :giver, :low_taker, :high_taker
-
-  def initialize(giver, low_taker, high_taker)
-    @giver = giver
-    @low_taker = low_taker
-    @high_taker = high_taker
-  end
-end
+Output = Struct.new(:name, :chip)
+Transaction = Struct.new(:giver, :low_type, :low_taker, :high_type, :high_taker)
 
 bots = Hash.new
 transactions = Hash.new
+outputs = Hash.new
 
 File.readlines("10.input").each do |line|
   if line.start_with?("value")
@@ -42,13 +36,24 @@ File.readlines("10.input").each do |line|
     bots[name].give(chip)
   elsif line.start_with?("bot")
     giver = line.split(" ")[1].to_i
+    low_type = line.split(" ")[5]
     low_taker = line.split(" ")[6].to_i
+    high_type = line.split(" ")[10]
     high_taker = line.split(" ")[11].to_i
 
-    transactions[giver] = Transaction.new(giver, low_taker, high_taker)
+    transactions[giver] = Transaction.new(giver, low_type, low_taker, high_type, high_taker)
 
-    bots[low_taker] = Bot.new(low_taker) unless bots[low_taker]
-    bots[high_taker] = Bot.new(high_taker) unless bots[high_taker]
+    if low_type == "bot"
+      bots[low_taker] = Bot.new(low_taker) unless bots[low_taker]
+    else
+      outputs[low_taker] = Output.new(low_taker)
+    end
+
+    if high_type == "bot"
+      bots[high_taker] = Bot.new(high_taker) unless bots[high_taker]
+    else
+      outputs[high_taker] = Output.new(high_taker)
+    end
   end
 end
 
@@ -69,7 +74,19 @@ until transactions.empty? do
 
     # perform transaction
     t = transactions.delete(giver_name)
-    bots[t.low_taker].give(giver.take_low)
-    bots[t.high_taker].give(giver.take_high)
+
+    if t.low_type == "bot"
+      bots[t.low_taker].give(giver.take_low)
+    else
+      outputs[t.low_taker].chip = giver.take_low
+    end
+
+    if t.high_type == "bot"
+      bots[t.high_taker].give(giver.take_high)
+    else
+      outputs[t.high_taker].chip = giver.take_high
+    end
   end
 end
+
+puts "Part 2: Multiplying chips of output 1,2,3: #{outputs[0].chip * outputs[1].chip * outputs[2].chip}"
